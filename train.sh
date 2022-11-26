b324ec521ac919095a85f64ae38e4375d4610531
Sangwon Lim <sangwon3@ualberta.ca>
2022-11-25 23:24:00 -0700

    bash script

---
+++ train.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python train.py --exp-name exp1 --model model1 --loss msie --KNNstep 1
+python train.py --exp-name exp2 --model model1 --loss focal --KNNstep 1
+python train.py --exp-name exp3 --model model1 --loss combined --KNNstep 1
+python train.py --exp-name exp4 --model model1 --loss msie
+python train.py --exp-name exp5 --model model1 --loss focal
+python train.py --exp-name exp6 --model model1 --loss combined
