9cb70e78c3fd78785ba384be31886df8c5e12e03
Sangwon Lim <sangwon3@ualberta.ca>
2022-11-28 13:02:53 -0700

    trilinear interpolation model2

--- models.py
+++ models.py
@@ -45,6 +45,7 @@ def upsample(downsampled_points, upsampled_points, device):
 
 
 class model1(nn.Module):
+    # Model with transformer interpolation in upsampling
     def __init__(self, *, batch_size, device):
         super().__init__()
         self.batch_size = batch_size
@@ -140,6 +141,109 @@ class model1(nn.Module):
         upsampled_sa4 = torch.stack(
             upsampled_sa4.flatten(start_dim=-2, end_dim=-1).split(512, dim=0), dim=0
         )
+        upsampled_sa4 = torch.cat((upsampled_sa4, sa2), axis=-1)
+
+        output = self.mlp3(upsampled_sa4)
+
+        return output
+
+
+class model2(nn.Module):
+    # Model with trilinear interpolation for upsampling
+    def __init__(self, *, batch_size, device):
+        super().__init__()
+        self.batch_size = batch_size
+        self.device = device
+
+        self.attn1 = PointTransformerLayer(
+            dim=3,
+            pos_mlp_hidden_dim=64,
+            attn_mlp_hidden_mult=4,
+        )
+        self.attn2 = PointTransformerLayer(
+            dim=16 * 3,
+            pos_mlp_hidden_dim=64,
+            attn_mlp_hidden_mult=4,
+            num_neighbors=16,
+        )
+        self.attn3 = PointTransformerLayer(
+            dim=3,
+            pos_mlp_hidden_dim=64,
+            attn_mlp_hidden_mult=4,
+        )
+        self.attn4 = PointTransformerLayer(
+            dim=16 * 3,
+            pos_mlp_hidden_dim=64,
+            attn_mlp_hidden_mult=4,
+        )
+        self.attn5 = PointTransformerLayer(
+            dim=3,
+            pos_mlp_hidden_dim=64,
+            attn_mlp_hidden_mult=4,
+        )
+        self.mlp1 = nn.Sequential(
+            nn.Linear(16 * 3, 16 * 3), nn.ReLU(), nn.Linear(16 * 3, 16 * 3), nn.ReLU()
+        )
+        self.mlp2 = nn.Sequential(
+            nn.Linear(16 * 3, 16 * 3), nn.ReLU(), nn.Linear(16 * 3, 1)
+        )
+        self.mlp3 = nn.Sequential(
+            nn.Linear(16 * 6, 16 * 3),
+            nn.ReLU(),
+            nn.Linear(16 * 3, 16 * 3),
+            nn.ReLU(),
+            nn.Linear(16 * 3, 1),
+        )
+        self.maxpool = nn.MaxPool2d((1, 16), stride=(1, 16))
+
+    def forward(self, original_points, data):
+        data = data.flatten(start_dim=0, end_dim=1)
+        # Self-attention of input points wrt target points
+        sa1 = self.attn1(data, data[:, :, 0:3])
+        sa1 = torch.stack(
+            sa1.flatten(start_dim=-2, end_dim=-1).split(512, dim=0), dim=0
+        )
+        sa1 = self.mlp1(sa1)
+
+        # Self-attention within target points
+        sa2 = self.attn2(sa1, original_points)
+        sa2 = self.mlp1(sa2)
+
+        # Downsampling
+        pivot_points, indices, _ = downsample(original_points, 128, self.device)
+        downsampled_sa2 = batched_index_select(sa2, indices).flatten(
+            start_dim=0, end_dim=1
+        )
+        neighbour_points = batched_index_select(original_points, indices).flatten(
+            start_dim=0, end_dim=1
+        )
+
+        sa3 = self.attn3(
+            self.maxpool(downsampled_sa2),
+            neighbour_points,
+        )
+        sa3 = torch.stack(
+            sa3.flatten(start_dim=-2, end_dim=-1).split(128, dim=0), dim=0
+        )
+        sa4 = self.attn4(
+            sa3,
+            pivot_points,
+        )
+
+        # Upsampling
+        indices, dist = upsample(pivot_points, original_points, self.device)
+        upsampled_sa4 = batched_index_select(sa4, indices)
+        # trilinear interpolation
+        dist_reverse = (
+            torch.max(dist, -1)[0].unsqueeze(-1).repeat([1, 1, 16]) - dist + 0.0001
+        )
+        dist_norm = dist_reverse / torch.sum(dist_reverse, -1).unsqueeze(-1).repeat(
+            [1, 1, 16]
+        )
+        dist_norm = dist_norm.unsqueeze(-1).repeat([1, 1, 1, 16 * 3])
+        upsampled_sa4 *= dist_norm
+        upsampled_sa4 = self.maxpool(upsampled_sa4)
+        upsampled_sa4 = upsampled_sa4.flatten(start_dim=-2, end_dim=-1)
         upsampled_sa4 = torch.cat((upsampled_sa4, sa2), axis=-1)
 
         output = self.mlp3(upsampled_sa4)
--- train.py
+++ train.py
@@ -8,13 +8,13 @@ import torch.optim as optim
 import sklearn.metrics as metrics
 from torch import nn
 import torch.nn.functional as F
-from models import model1
+from models import model1, model2
 from utils import LidarData, MSIE_Loss, Focal_Loss, combined_Loss
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import DataLoader
 
 
-model_dict = {"model1": model1}
+model_dict = {"model1": model1, "model2": model2}
 loss_dict = {
     "mse": nn.MSELoss,
     "msie": MSIE_Loss,
