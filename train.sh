1b495399c9f42ddebd5c107545423601345b34fb
Sangwon Lim <sangwon3@ualberta.ca>
2022-11-28 15:29:10 -0700

    fix typo

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python train.py --exp-name exp1 --model model0_1 --loss msie --KNNstep 1
-python train.py --exp-name exp2 --model model0_2 --loss msie --KNNstep 1
-python train.py --exp-name exp3 --model model1_1 --loss msie --KNNstep 1
-python train.py --exp-name exp4 --model model1_2 --loss msie --KNNstep 1
-python train.py --exp-name exp5 --model model2_1 --loss msie --KNNstep 1
-python train.py --exp-name exp6 --model model2_2 --loss msie --KNNstep 1
+python train.py --exp-name exp1 --model model0_0 --loss msie --KNNstep 1
+python train.py --exp-name exp2 --model model0_1 --loss msie --KNNstep 1
+python train.py --exp-name exp3 --model model1_0 --loss msie --KNNstep 1
+python train.py --exp-name exp4 --model model1_1 --loss msie --KNNstep 1
+python train.py --exp-name exp5 --model model2_0 --loss msie --KNNstep 1
+python train.py --exp-name exp6 --model model2_1 --loss msie --KNNstep 1
