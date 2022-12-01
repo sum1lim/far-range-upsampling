fe7230591b102d570e269211ab67e30a695d380d
Sangwon Lim <sangwon3@ualberta.ca>
2022-12-01 11:12:39 -0700

    patience = 50

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python train.py --exp-name exp1 --model model0_0 --loss msie --KNNstep 1 --patience 100
+python train.py --exp-name exp1 --model model0_0 --loss msie --KNNstep 1
 python train.py --exp-name exp2 --model model0_1 --loss msie --KNNstep 1
 python train.py --exp-name exp3 --model model1 --loss msie --KNNstep 1
 python train.py --exp-name exp4 --model model2 --loss msie --KNNstep 1
