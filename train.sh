f755e8891c996c27d80c4303a11f00dc69527a59
Sangwon Lim <sangwon3@ualberta.ca>
2022-12-02 14:30:57 -0700

    KNN steps

--- train.sh
+++ train.sh
@@ -4,5 +4,6 @@ python train.py --exp-name exp2 --model model0_1 --loss msie --KNNstep 1
 python train.py --exp-name exp3 --model model1 --loss msie --KNNstep 1
 python train.py --exp-name exp4 --model model2 --loss msie --KNNstep 1
 
-python train.py --exp-name exp5 --model model1 --loss msie --KNNstep 4
-python train.py --exp-name exp6 --model model1 --loss msie --KNNstep 8
+python train.py --exp-name exp5 --model model1 --loss msie --KNNstep 2
+python train.py --exp-name exp6 --model model1 --loss msie --KNNstep 4
+python train.py --exp-name exp7 --model model1 --loss msie --KNNstep 8
