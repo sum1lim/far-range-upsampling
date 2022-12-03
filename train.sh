fa21be49cdfc33e0c10f83122ee105d6a8f5f628
Sangwon Lim <sangwon3@ualberta.ca>
2022-12-03 10:24:30 -0700

    more experiments

--- train.sh
+++ train.sh
@@ -7,3 +7,7 @@ python train.py --exp-name exp4 --model model2 --loss msie --KNNstep 1
 python train.py --exp-name exp5 --model model1 --loss msie --KNNstep 2
 python train.py --exp-name exp6 --model model1 --loss msie --KNNstep 4
 python train.py --exp-name exp7 --model model1 --loss msie --KNNstep 8
+
+python train.py --exp-name exp8 --model model1 --loss mse --KNNstep 1
+python train.py --exp-name exp9 --model model1 --loss focal --KNNstep 1
+python train.py --exp-name exp10 --model model1 --loss combined --KNNstep 1
